e49eb66c9cb109e8d9ff928ca8445730b48cd696
Tal Rasha <talrasha007@gmail.com>
2018-12-08 23:51:32 +0800

    update deploy script.

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,9 +1,17 @@
-// const fs = require('fs');
-
 const HashedgeFactory = artifacts.require('./HashedgeFactory.sol');
 
 module.exports = async function(deployer) {
   deployer.then(async () => {
     await deployer.deploy(HashedgeFactory);
+
+    const hashedgeFactory = require('../build/contracts/HashedgeFactory').abi;
+    const hashRateOptionsToken = require('../build/contracts/HashRateOptionsToken').abi;
+    const uniswapExchange = require('../build/contracts/UniswapExchange').abi;
+
+    console.log(      __dirname + '/../build/abi.json');
+    require('fs').writeFileSync(
+      __dirname + '/../build/abi.json',
+      new Buffer(JSON.stringify({ hashedgeFactory, hashRateOptionsToken, uniswapExchange }, 2, 2))
+    );
   });
 };
