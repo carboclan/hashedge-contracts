3dd3a81c8e56a25a1f413d035e12a58957a01987
Tal Rasha <talrasha007@gmail.com>
2018-12-08 21:27:23 +0800

    exchange & options token creation.

--- contracts/HashedgeFactory.sol
+++ contracts/HashedgeFactory.sol
@@ -18,17 +18,52 @@ contract HashedgeFactory is FactoryInterface {
 
   // index of tokens with registered exchanges
   address[] public tokenList;
+  mapping(string => address) symbolToToken;
   mapping(address => address) tokenToExchange;
   mapping(address => address) exchangeToToken;
 
-  function createExchange(address _token) public returns (address exchange) {
-    require(tokenToExchange[_token] == address(0));             //There can only be one exchange per token
-    require(_token != address(0) && _token != address(this));
-    UniswapExchange newExchange = new UniswapExchange(_token);
-    tokenList.push(_token);
-    tokenToExchange[_token] = newExchange;
-    exchangeToToken[newExchange] = _token;
-    emit ExchangeCreated(newExchange, _token);
+  function createExchange(
+    uint256 _target, string _name, string _symbol, uint8 _decimals, uint256 _totalSupply,
+    string _hashType, string _currencyType, string _hashRateUnit, uint256 _tokenSize,
+    uint256 _startTs, uint256 _endTs, uint256 _strikePrice
+  ) public returns (address exchange) {
+    require(symbolToToken[_symbol] == address(0));
+    require(_target > 0);
+    require(bytes(_name).length > 0);
+    require(bytes(_symbol).length > 0);
+    require(_decimals > 0);
+    require(_totalSupply > 0);
+    require(bytes(_hashType).length > 0);
+    require(bytes(_currencyType).length > 0);
+    require(bytes(_hashRateUnit).length > 0);
+    require(_tokenSize > 0);
+    require(_startTs > now);
+    require(_endTs > _startTs);
+    require(_strikePrice > 0);
+
+    HashRateOptionsToken token = new HashRateOptionsToken(
+      _name,
+      _symbol,
+      _decimals
+    );
+
+    token.setInfo(
+      _hashType,
+      _currencyType,
+      _hashRateUnit,
+      _tokenSize,
+      _startTs,
+      _endTs,
+      _strikePrice
+    );
+
+    UniswapExchange newExchange = new UniswapExchange(token, _target, _totalSupply);
+    token.transferOwnership(newExchange);
+    tokenList.push(token);
+    symbolToToken[_symbol] = token;
+    tokenToExchange[token] = newExchange;
+    exchangeToToken[newExchange] = token;
+    emit ExchangeCreated(newExchange, token);
     return newExchange;
   }
 
@@ -42,5 +77,5 @@ contract HashedgeFactory is FactoryInterface {
 
   function exchangeToTokenLookup(address _exchange) public view returns (address token) {
     return exchangeToToken[_exchange];
- }
+  }
 }
