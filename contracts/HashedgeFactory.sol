d2dd774ccac11fecc631f4b74cbd3f68b3eb4ca1
Tal Rasha <talrasha007@gmail.com>
2018-12-08 21:09:47 +0800

    just copy the uniswap logic to here. wait for modify.

--- contracts/HashedgeFactory.sol
+++ contracts/HashedgeFactory.sol
@@ -1,5 +1,7 @@
 pragma solidity ^0.4.24;
 
+import "./UniswapExchange.sol";
+
 contract FactoryInterface {
   address[] public tokenList;
   mapping(address => address) tokenToExchange;
@@ -12,5 +14,33 @@ contract FactoryInterface {
 }
 
 contract HashedgeFactory is FactoryInterface {
+  event ExchangeCreated(address indexed exchange, address indexed token);
 
+  // index of tokens with registered exchanges
+  address[] public tokenList;
+  mapping(address => address) tokenToExchange;
+  mapping(address => address) exchangeToToken;
+
+  function createExchange(address _token) public returns (address exchange) {
+    require(tokenToExchange[_token] == address(0));             //There can only be one exchange per token
+    require(_token != address(0) && _token != address(this));
+    UniswapExchange newExchange = new UniswapExchange(_token);
+    tokenList.push(_token);
+    tokenToExchange[_token] = newExchange;
+    exchangeToToken[newExchange] = _token;
+    emit ExchangeCreated(newExchange, _token);
+    return newExchange;
+  }
+
+  function getExchangeCount() public view returns (uint exchangeCount) {
+    return tokenList.length;
+  }
+
+  function tokenToExchangeLookup(address _token) public view returns (address exchange) {
+    return tokenToExchange[_token];
+  }
+
+  function exchangeToTokenLookup(address _exchange) public view returns (address token) {
+    return exchangeToToken[_exchange];
+ }
 }
