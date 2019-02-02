3f6c1162586b7e02410d4af5460d7d4669f65a3c
Tal Rasha <talrasha007@gmail.com>
2019-02-02 18:12:48 +0800

    truffle config.

--- truffle-config.js
+++ truffle-config.js
@@ -1,18 +1,32 @@
-/*
- * NB: since truffle-hdwallet-provider 0.0.5 you must wrap HDWallet providers in a 
- * function when declaring them. Failure to do so will cause commands to hang. ex:
- * ```
- * mainnet: {
- *     provider: function() { 
- *       return new HDWalletProvider(mnemonic, 'https://mainnet.infura.io/<infura-key>') 
- *     },
- *     network_id: '1',
- *     gas: 4500000,
- *     gasPrice: 10000000000,
- *   },
- */
+function getPrivateKeyProvider() {
+  const PrivateKeyProvider = require('truffle-privatekey-provider');
+  return new PrivateKeyProvider(process.env.PRI, process.env.URL || 'https://ropsten.infura.io/8Q30zi3TAqlJ6JSCwcul');
+}
 
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+  networks: {
+    testrpc: {
+      host: "localhost",
+      port: 8545,
+      network_id: "*",
+      gas: 4500000, // Gas limit used for deploys
+      gasPrice: 10000000000 // 10 gwei
+    },
+    production: {
+      provider: process.env.PRI && getPrivateKeyProvider(),
+      network_id: '*',
+      gas: 4500000, // Gas limit used for deploys
+      gasPrice: 10000000000 // 10 gwei
+    }
+  },
+  solc: {
+    optimizer: {
+      enabled: true,
+      runs: 200
+    }
+  },
+  // https://truffle.readthedocs.io/en/beta/advanced/configuration/
+  mocha: {
+    bail: true
+  }
 };
